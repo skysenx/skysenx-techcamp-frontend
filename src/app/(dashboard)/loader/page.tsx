90da47c7c3c722e9296cdfcee3c0abce6b9be7fb
Tubolayefa <tlayefaah@gmail.com>
2025-06-27 17:30:36 +0100

    feat: add output configuration option in next.config.ts and remove unused loader component

--- src/app/(dashboard)/loader/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function page() {
-  return (
-    <div className="h-full flex justify-center items-center">
-      <div className="loader"></div>
-    </div>
-  );
-}
